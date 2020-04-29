90cc9552c492fbd5f51a7c2a6c8e32e5207205c3
mubojia <mubojia@co-mall.com>
2020-04-29 09:07:33 +0800

    第一个案例demo

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
- * @format
+ * @format 入口
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
